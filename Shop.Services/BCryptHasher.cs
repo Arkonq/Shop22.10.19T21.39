77c718657344c3a16b350a6d5c481b806bdea4a1
arkonq@gmail.com <arkonq@gmail.com>
2019-10-22 21:40:06 +0600

    Add project files.

---
+++ Shop.Services/BCryptHasher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Shop.Services.Abstract;
+using BCrypt.Net;
+using static BCrypt.Net.BCrypt;
+
+namespace Shop.Services
+{
+	public class BcryptHasher : ICryptoService
+	{
+		public string EncryptPassword(string password)
+		{
+			return HashPassword(password);
+		}
+
+		public bool VerifyPassword(string password, string passwordCandidate)
+		{
+			return Verify(passwordCandidate, password);
+		}
+	}
+}
